0de7ac6d9a1bdbeaedf5855c7034a2e70c97aed5
Sebastián Ibañez <77544506+Z3belek@users.noreply.github.com>
2022-05-03 20:57:30 -0300

    Database

---
+++ Database/PortfolioAP.sql
@@ -0,0 +1,91 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.4.24-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.0.0.6468
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Volcando estructura de base de datos para portfolioap
+CREATE DATABASE IF NOT EXISTS `portfolioap` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `portfolioap`;
+
+-- Volcando estructura para tabla portfolioap.zz_aboutme
+CREATE TABLE IF NOT EXISTS `zz_aboutme` (
+  `Id` int(10) unsigned NOT NULL,
+  `Name` text NOT NULL,
+  `DTexto` longtext NOT NULL,
+  `Skill1` text NOT NULL,
+  `Skill2` text NOT NULL,
+  `Skill3` text NOT NULL,
+  `Percentage1` tinyint(4) NOT NULL DEFAULT 0,
+  `Percentage2` tinyint(4) NOT NULL DEFAULT 0,
+  `Percentage3` tinyint(4) NOT NULL DEFAULT 0,
+  `Urlselfie` text NOT NULL,
+  `Urlcv` text NOT NULL,
+  `Linkedin` text NOT NULL,
+  `Github` text NOT NULL,
+  `Mail` text NOT NULL,
+  `Pinterest` text NOT NULL,
+  `PCompletados` smallint(6) NOT NULL,
+  `CTomados` smallint(6) NOT NULL,
+  `CSatisfactorios` smallint(6) NOT NULL,
+  `Premios` smallint(6) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla portfolioap.zz_education
+CREATE TABLE IF NOT EXISTS `zz_education` (
+  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Year1` tinytext NOT NULL,
+  `Year2` tinytext NOT NULL,
+  `Level` tinytext NOT NULL,
+  `Place` text NOT NULL,
+  `DTexto` text NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla portfolioap.zz_experience
+CREATE TABLE IF NOT EXISTS `zz_experience` (
+  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Year1` tinytext NOT NULL,
+  `Year2` tinytext NOT NULL,
+  `Role` tinytext NOT NULL,
+  `Place` text NOT NULL,
+  `DTexto` text NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla portfolioap.zz_project
+CREATE TABLE IF NOT EXISTS `zz_project` (
+  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` tinytext NOT NULL,
+  `type` tinytext NOT NULL,
+  `place` text NOT NULL,
+  `dtexto` text NOT NULL,
+  `imgurl` text NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
