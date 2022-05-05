b7e785d18dd89eb72db611c47387df95370d7753
Sebastián Ibañez <77544506+Z3belek@users.noreply.github.com>
2022-05-05 04:03:38 -0300

    Update db

--- Database/PortfolioAP.sql
+++ Database/PortfolioAP.sql
@@ -15,11 +15,11 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Volcando estructura de base de datos para portfolioap
-CREATE DATABASE IF NOT EXISTS `portfolioap` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `portfolioap`;
+-- Volcando estructura de base de datos para portfolioap2
+CREATE DATABASE IF NOT EXISTS `portfolioap2` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `portfolioap2`;
 
--- Volcando estructura para tabla portfolioap.zz_aboutme
+-- Volcando estructura para tabla portfolioap2.zz_aboutme
 CREATE TABLE IF NOT EXISTS `zz_aboutme` (
   `Id` int(10) unsigned NOT NULL,
   `Name` text NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `zz_aboutme` (
 
 -- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla portfolioap.zz_education
+-- Volcando estructura para tabla portfolioap2.zz_education
 CREATE TABLE IF NOT EXISTS `zz_education` (
   `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Year1` tinytext NOT NULL,
@@ -54,11 +54,11 @@ CREATE TABLE IF NOT EXISTS `zz_education` (
   `Place` text NOT NULL,
   `DTexto` text NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 -- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla portfolioap.zz_experience
+-- Volcando estructura para tabla portfolioap2.zz_experience
 CREATE TABLE IF NOT EXISTS `zz_experience` (
   `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Year1` tinytext NOT NULL,
@@ -71,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `zz_experience` (
 
 -- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla portfolioap.zz_project
+-- Volcando estructura para tabla portfolioap2.zz_project
 CREATE TABLE IF NOT EXISTS `zz_project` (
   `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` tinytext NOT NULL,
@@ -80,6 +80,40 @@ CREATE TABLE IF NOT EXISTS `zz_project` (
   `dtexto` text NOT NULL,
   `imgurl` text NOT NULL,
   PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla portfolioap2.zz_roles
+CREATE TABLE IF NOT EXISTS `zz_roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla portfolioap2.zz_users
+CREATE TABLE IF NOT EXISTS `zz_users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKk9ga0t44hkj7pf7p9cjb9xr6u` (`username`),
+  UNIQUE KEY `UK6x7hstc0hpxr7mt22ysib835x` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla portfolioap2.zz_user_roles
+CREATE TABLE IF NOT EXISTS `zz_user_roles` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `FKps2abni7tp0agtro82x60t9ac` (`role_id`),
+  CONSTRAINT `FKamukfc87xo6jdex4nl94pjjrm` FOREIGN KEY (`user_id`) REFERENCES `zz_users` (`id`),
+  CONSTRAINT `FKps2abni7tp0agtro82x60t9ac` FOREIGN KEY (`role_id`) REFERENCES `zz_roles` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- La exportación de datos fue deseleccionada.
